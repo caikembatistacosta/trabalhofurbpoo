501b2f5e3e10af47eec3fe3766170d6ddd697cfc
Caike Costa <cmbcosta@furb.br>
2023-02-13 19:34:42 -0300

    Trabalho POO FURB - 2022

---
+++ Marco4/src/module_japao/teste.java
@@ -0,0 +1,13 @@
+package module_japao;
+
+import fifa.NationalTeamStats;
+
+public class teste {
+
+	public static void main(String[] args) {
+
+		Japao j = new Japao();
+		j.getAverageAge();
+	}
+
+}
